0ea5614d18ead1735ed82d71a26aa615b5fc8e11
Zhengqian Kuang <dev.kuang@gmail.com>
2021-02-25 21:06:09 -0600

    useEffect, useLayoutEffect

--- components/demo/hooks/table-of-contents/index.tsx
+++ components/demo/hooks/table-of-contents/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {createStackNavigator} from '@react-navigation/stack';
 import {View, Text} from 'react-native';
 import {FlatList, TouchableOpacity} from 'react-native-gesture-handler';
 import {styles} from './style';
 import {useNavigation} from '@react-navigation/native';
+
+// https://reactjs.org/docs/hooks-reference.html
 
 type HooksDemoItem = 'useState'
 export type HooksDemoScreenName = 'UseStateDemo';
@@ -21,7 +22,7 @@ const hooksDemoList: HooksDemoListItem[] = [
     index: 0,
     id: 'useState',
     navigationTargetName: 'UseStateDemo',
-    title: 'useState',
+    title: 'use-State/Effect/LayoutEffect',
   },
 ];
 
--- components/demo/hooks/useState/index.tsx
+++ components/demo/hooks/useState/index.tsx
@@ -4,6 +4,14 @@ import {styles, color} from './style';
 import {useNavigation} from '@react-navigation/native';
 import {TouchableOpacity} from 'react-native-gesture-handler';
 
+// https://daveceddia.com/useeffect-vs-uselayouteffect/
+// https://kentcdodds.com/blog/useeffect-vs-uselayouteffect
+// useEffect - this runs after react renders your component and ensures that your effect callback does not block browser painting
+// useLayoutEffect - if your effect is mutating the DOM (via a DOM node ref) and the DOM mutation will change the appearance of
+//   the DOM node between the time that it is rendered and your effect mutates it, then you don't want to use useEffect. You'll
+//   want to use useLayoutEffect. Otherwise the user could see a flicker when your DOM mutations take effect. This is pretty much
+//   the only time you want to avoid useEffect and use useLayoutEffect instead.
+
 export const DemoUseStateView = () => {
   type State = 'Welcome' | 'Order Placed' | 'Food Consumed' | 'Bill Paid';
   const [state, setState] = React.useState<State>('Welcome');
@@ -11,25 +19,29 @@ export const DemoUseStateView = () => {
   const navigation = useNavigation();
 
   React.useLayoutEffect(() => {
+    console.log('DemoUseStateView useLayoutEffect')
+
     navigation.setOptions({
       title: 'Virtual Restaurant',
       headerTitleStyle: {
         alignSelf: 'center',
       },
     });
-  }, [navigation, state]);
+  }, [navigation]);
 
   React.useEffect(() => {
     console.log(
-      'DemoUseStateView mounted -- true only when the dependencies is []',
+      'DemoUseStateView componentDidMount -- this is true only when the dependencies is []',
     );
 
     return () => {
-      console.log('DemoUseStateView unmounted');
+      console.log('DemoUseStateView componentWillUnmount');
     };
   }, []);
 
   React.useEffect(() => {
+    console.log('DemoUseStateView componentDidUpdate');
+
     navigation.removeListener('beforeRemove', () => {});
     navigation.addListener('beforeRemove', (e) => {
       if (state === 'Welcome' || state === 'Bill Paid') {
