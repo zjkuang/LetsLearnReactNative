3a4f207ff8d2118acc85f883bdfbc6617b3899bc
John Kuang <jkuang@gasbuddy.com>
2021-07-16 17:38:33 -0600

    styles

---
+++ doc/cheatsheets/cheatsheet-styles.ts
@@ -0,0 +1,11 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  centerContentsContainer: {
+    flex: 1,
+    alignItems: 'center', // along the direction perpendicular to flexDirection
+    justifyContent: 'center', // along flexDirection
+  },
+});
+
+export {styles};
