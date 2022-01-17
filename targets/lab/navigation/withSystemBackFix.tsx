f3494dbd7619510b964dac19a2c2a6c013c6f26b
John Kuang <jkuang@gasbuddy.com>
2022-01-16 20:50:24 -0600

    fix split-view js->ts things

--- targets/lab/navigation/withSystemBackFix.tsx
+++ targets/lab/navigation/withSystemBackFix.tsx
@@ -4,12 +4,12 @@ import {useSystemBack} from './useSystemBack';
 
 // JavaScript example: https://github.com/zjkuang/react-native-split-view-demo
 
-export const withSystemBackFix = Comp =>
-  Platform.select({
+export function withSystemBackFix(Comp: any) {
+  return Platform.select({
     android: React.forwardRef((props, ref) => {
       useSystemBack();
-
       return <Comp ref={ref} {...props} />;
     }),
     default: Comp,
   });
+}
